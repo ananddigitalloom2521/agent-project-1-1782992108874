11fc1272f9a5a4816e0c2e9b5727def8fe00eed8
ananddigitalloom2521 <ananddigitalloom@gmail.com>
2026-07-02 17:05:12 +0530

    Add src/index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes/index');
+
+app.use('/api', routes);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
